0d9e6993e32c149b166aa6cd988c2d51d9463cc1
daniell escareno <daniellescareno7@gmail.com>
2026-04-17 08:17:54 -0700

    Initial Commit

---
+++ Note Cards/Note_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_CardsApp.swift
+//  Note Cards
+//
+//  Created by daniell Escareno on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Note_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
