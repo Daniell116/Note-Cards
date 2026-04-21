f07f7da55aff9e2d8fbd8bbb4a007c5eb1b07e5e
daniell escareno <daniellescareno7@gmail.com>
2026-04-21 08:37:00 -0700

    made a Basic set up

--- Note Cards/ContentView.swift
+++ Note Cards/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isfillped = false
+    @State private var addCards = false
     var body: some View {
-        VStack {
-            Text("Flash Cards")
-                .font(.largeTitle)
-                .bold()
-            Spacer()
+        NavigationView {
+            VStack {
+                List {
+                    
+                }
+                // incase needed
+            }
+            
+            .navigationBarTitle("Note Card Sets", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                addCards = true
+            }, label: {
+                Image(systemName: "plus")
+            }))
+                
+            }
         }
-        .padding()
     }
-}
+
 
 #Preview {
     ContentView()
