482479f000973b35c16771979145a8ca74594732
daniell escareno <daniellescareno7@gmail.com>
2026-04-22 12:18:43 -0700

    fixed text so now diffrent when flipped

--- Note Cards/NoteCardView.swift
+++ Note Cards/NoteCardView.swift
@@ -8,28 +8,32 @@
 import SwiftUI
 
 struct NoteCardView: View {
-    @State private var question = ""
-    @State private var answer = ""
+    @State private var question: String
+    @State private var answer: String
     @State private var showAnswer = false
+    
+    init(question: String, answer: String) {
+        self._question = State(initialValue: question)
+        self._answer = State(initialValue: answer)
+    }
+    
     var body: some View {
         VStack {
             ZStack {
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color.blue.opacity(0.2))
                     .frame(height: 200)
-                Text(question)
-                    .font(.title)
                 Text(showAnswer ? answer : question)
                     .font(.title)
                     .foregroundColor(.black)
                     .padding()
             }
             .rotation3DEffect(
-                .degrees(showAnswer ? 180 : 0),
+                .degrees(showAnswer ? 360 : 0),
                 axis : (x:0, y: 1, z: 0)
             )
             // broken but will fix 
-            .animation(.easeIn(duration: 0.5),value: showAnswer)
+            .animation(.easeIn(duration: 0.6),value: showAnswer)
             .onTapGesture {
                 showAnswer.toggle()
             }
@@ -43,5 +47,5 @@ struct NoteCardView: View {
 }
 
 #Preview {
-    NoteCardView()
+    NoteCardView(question: "", answer: "")
 }
