0b26dbbadafe1b89b4c88db6014d5877f15f1000
daniell escareno <daniellescareno7@gmail.com>
2026-04-22 08:38:00 -0700

    Created Note card view

---
+++ Note Cards/NoteCardView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteCard View.swift
+//  Note Cards
+//
+//  Created by daniell Escareno on 4/22/26.
+//
+
+import SwiftUI
+
+struct NoteCardView: View {
+    @State private var question = ""
+    @State private var answer = ""
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteCardView()
+}
